713bc7265324f981c4d32f2e57d05edc2e01db72
Yelshat Duskaliyev <ura2178@gmail.com>
2023-03-27 11:51:19 +0300

    feat: use storyboards instead of swift ui (temporarily)

--- arucopublisher/ContentView.swift
+++ arucopublisher/ContentView.swift
@@ -5,191 +5,186 @@
 //  Created by Yelshat Duskaliyev on 27.03.2023.
 //
 
+import UIKit
+import SceneKit
+import ARKit
 import SwiftUI
-import AVFoundation
+import SceneKit
 
-struct ArucoMarkerPreview: UIViewRepresentable {
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
+let ArucoMarkerSize: Float = 0.133
 
-    func makeUIView(context: Context) -> UIView {
-        let arucoMarkerViewController = ArucoMarkerViewController()
-        return arucoMarkerViewController.view
-    }
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate, ARSessionObserver {
 
-    func updateUIView(_ uiView: UIView, context: Context) {
-    }
-
-    class Coordinator: NSObject {
-        var parent: ArucoMarkerPreview
-
-        init(_ parent: ArucoMarkerPreview) {
-            self.parent = parent
-        }
-    }
-
-}
-
-struct ContentView: View {
-    @State private var isCameraAuthorized = false
-
-    var body: some View {
-        VStack {
-            if isCameraAuthorized {
-                ArucoMarkerPreview()
-                        .edgesIgnoringSafeArea(.all)
-            } else {
-                Text("Camera access required")
-            }
-        }.onAppear { checkCameraAuthorization()}
-    }
-
-    func checkCameraAuthorization() {
-        switch AVCaptureDevice.authorizationStatus(for: .video) {
-        case .authorized:
-            isCameraAuthorized = true
-        case .notDetermined:
-            AVCaptureDevice.requestAccess(for: .video) { granted in
-                DispatchQueue.main.async {
-                    isCameraAuthorized = granted
-                }
-            }
-        default:
-            break
-        }
-    }
-
-}
-
-class ArucoMarkerViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
-    private var captureSession: AVCaptureSession!
-    private var previewLayer: AVCaptureVideoPreviewLayer!
-    private var intrinsics: [NSNumber]!
-    private var distortionCoefficients: [NSNumber]!
-    private let markerSize: Float64 = 0.133
+    @IBOutlet var sceneView: ARSCNView!
+    var mutexlock = false;
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        captureSession = AVCaptureSession()
-        captureSession.sessionPreset = .high
+        sceneView.showsStatistics = true
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
 
-        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
-            return
-        }
-        let videoInput: AVCaptureDeviceInput
-
-        // set intrinsics and distortion coefficients
-        let cameraIntrinsics = getCameraIntrinsics(from: videoCaptureDevice)
-        intrinsics = cameraIntrinsics.intrinsics
-        distortionCoefficients = cameraIntrinsics.distortionCoefficients
-
-        NSLog("Camera intrinsics: \(cameraIntrinsics)")
-
-        do {
-            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
-        } catch {
-            return
-        }
-
-        if !captureSession.canAddInput(videoInput) {
-            return
-        }
-
-        captureSession.addInput(videoInput)
-        let videoOutput = AVCaptureVideoDataOutput()
-        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
-
-        if captureSession.canAddOutput(videoOutput) {
-            return
-        }
-
-        captureSession.addOutput(videoOutput)
-
-        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        previewLayer.frame = view.layer.bounds
-        previewLayer.videoGravity = .resizeAspectFill
-        view.layer.addSublayer(previewLayer)
-
-        DispatchQueue.global(qos: .userInitiated).async {
-            self.captureSession.startRunning()
-        }
+        sceneView.delegate = self
+        sceneView.session.delegate = self
     }
 
-    func getCameraIntrinsics(from captureDevice: AVCaptureDevice) -> (intrinsics: [NSNumber], distortionCoefficients: [NSNumber]) {
-        let intrinsics = captureDevice.activeFormat.videoSupportedFrameRateRanges.first!.minFrameRate
-        let distortionCoefficients = captureDevice.activeFormat.videoSupportedFrameRateRanges.first!.maxFrameRate
-        return (
-                [
-                    NSNumber(value: intrinsics),
-                    NSNumber(value: intrinsics),
-                    NSNumber(value: intrinsics)
-                ],
-                [
-                    NSNumber(value: distortionCoefficients),
-                    NSNumber(value: distortionCoefficients),
-                    NSNumber(value: distortionCoefficients)
-                ]
-        )
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        configuration.isLightEstimationEnabled = true
+        configuration.worldAlignment = .gravity
+
+        // Run the view's session
+        sceneView.autoenablesDefaultLighting = true;
+        sceneView.session.run(configuration)
     }
 
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        NSLog("captureOutput")
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
 
-        connection.videoOrientation = .portrait
-
-        guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
-            return
-        }
-
-        NSLog("requesting markers")
-
-        let markers = OpenCVWrapper.detectAndLocalize(imageBuffer,
-                withIntrinsics: intrinsics,
-                distortionCoefficients: distortionCoefficients,
-                markerSize: markerSize)
-
-        NSLog("no. of markers: \((markers as! [ArucoMarker]).count)")
-
-        // Process detected Aruco markers and draw them on the preview screen
-        DispatchQueue.main.async {
-            self.drawArucoMarkers(markers: markers as! [ArucoMarker])
-        }
+        // Pause the view's session
+        sceneView.session.pause()
     }
 
-    private func drawArucoMarkers(markers: [ArucoMarker]) {
-        view.subviews.forEach {
-            if $0.tag == 100 {
-                $0.removeFromSuperview()
+    func updateContentNodeCache(targTransforms: Array<SKWorldTransform>, cameraTransform: SCNMatrix4) {
+
+        for transform in targTransforms {
+
+            let targTransform = SCNMatrix4Mult(transform.transform, cameraTransform);
+
+            if let box = findCube(arucoId: Int(transform.arucoId)) {
+                box.setWorldTransform(targTransform);
+
+            } else {
+
+                let arucoCube = ArucoNode(arucoId: Int(transform.arucoId))
+                sceneView.scene.rootNode.addChildNode(arucoCube);
+                arucoCube.setWorldTransform(targTransform);
             }
         }
+    }
 
-        for marker in markers {
-            let points = [marker.corners.first, marker.corners.second, marker.corners.third, marker.corners.fourth]
-            let minX = points.map {
-                        $0.x
-                    }
-                    .min()!
-            let minY = points.map {
-                        $0.y
-                    }
-                    .min()!
-            let maxX = points.map {
-                        $0.x
-                    }
-                    .max()!
-            let maxY = points.map {
-                        $0.y
-                    }
-                    .max()!
-
-            let rect = CGRect(x: minX, y: minY, width: maxX - minX, height: maxY - minY)
-            let markerView = UIView(frame: rect)
-            markerView.layer.borderColor = UIColor.red.cgColor
-            markerView.layer.borderWidth = 2
-            markerView.tag = 100
-            view.addSubview(markerView)
+    func findCube(arucoId: Int) -> ArucoNode? {
+        for node in sceneView.scene.rootNode.childNodes {
+            if node is ArucoNode {
+                let box = node as! ArucoNode
+                if (arucoId == box.id) {
+                    return box
+                }
+            }
         }
+        return nil
+    }
+
+    // MARK: - ARSessionDelegate
+
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+
+        if self.mutexlock {
+            return;
+        }
+
+        self.mutexlock = true;
+        let pixelBuffer = frame.capturedImage
+
+        // 1) cv::aruco::detectMarkers
+        // 2) cv::aruco::estimatePoseSingleMarkers
+        // 3) transform offset and rotation of marker's corners in OpenGL coords
+        // 4) return them as an array of matrixes
+
+        let transMatrixArray: Array<SKWorldTransform> = OpenCVWrapper.detectAndLocalize(
+                pixelBuffer,
+                withIntrinsics: frame.camera.intrinsics,
+                distortionCoefficients: [Float](repeating: 0, count: 8),
+                markerSize: Float64(ArucoMarkerSize)
+        ) as! Array<SKWorldTransform>;
+
+
+        if (transMatrixArray.count == 0) {
+            self.mutexlock = false;
+            return;
+        }
+
+        let cameraMatrix = SCNMatrix4.init(frame.camera.transform);
+
+        DispatchQueue.main.async(execute: {
+            self.updateContentNodeCache(targTransforms: transMatrixArray, cameraTransform: cameraMatrix)
+
+            self.mutexlock = false;
+        })
+    }
+
+    func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+    }
+
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+//        NSLog("%s", __FUNC__)
+    }
+
+    func session(_ session: ARSession, didRemove anchors: [ARAnchor]) {
+    }
+
+    // MARK: - ARSessionObserver
+
+    func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+    }
+
+    // MARK: - ARSCNViewDelegate
+
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+
+        return node
+    }
+*/
+
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+    }
+
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    }
+
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
     }
 }
+
+struct SKWorldTransform {
+    let arucoId: Int
+    let transform: SCNMatrix4
+}
+
+import Foundation
+import ARKit
+
+class ArucoNode : SCNNode {
+    var size:CGFloat;
+    public let id:Int;
+
+    init(sz:CGFloat = 0.04, arucoId:Int = 23) {
+        self.size = CGFloat(ArucoMarkerSize);
+        self.id = arucoId;
+
+        super.init();
+
+        self.geometry = SCNBox(width: size, height: size, length: size, chamferRadius: 0)
+        let mat = SCNMaterial()
+        self.geometry?.materials = [mat]
+
+        let hue = CGFloat((id * 3) % 250);
+        let color: UIColor = UIColor.colorWithHSV(hue: hue, saturation: 1, value: 1)!
+        mat.diffuse.contents = color
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
