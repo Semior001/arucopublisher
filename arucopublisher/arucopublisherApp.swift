045886eac8d36c64af5a6d63a98667aa92266f0c
Yelshat Duskaliyev <ura2178@gmail.com>
2023-03-27 03:24:08 +0300

    Initial Commit

---
+++ arucopublisher/arucopublisherApp.swift
@@ -0,0 +1,17 @@
+//
+//  arucopublisherApp.swift
+//  arucopublisher
+//
+//  Created by Yelshat Duskaliyev on 27.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct arucopublisherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
